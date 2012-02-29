96c67bac2bc33477b85fc5ff08c8686b5c94d590
Michael Patterson <mjpatter88@gmail.com>
2012-02-29 15:09:41 -0600

    Updated the readme and added a new directory to hold files for a software simulation on the convey machine.  Added the first two source scripts, still need to add the code files.

---
+++ convey/sobel/convey_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export CNY_PDK_HDLSIM=Mentor
+export CNY_PDK_SIMMODE=64
---
+++ convey/sobel/xilinx_setup.sh
@@ -0,0 +1,3 @@
+source /remote/Xilinx/12.4/settings64.sh
+export PATH=$PATH:/remote/Modelsim/6.5c/modeltech/linux_x86_64/
+export LM_LICENSE_FILE=1717@io.ece.iastate.edu:27006@io.ece.iastate.edu
